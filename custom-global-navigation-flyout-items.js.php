c51eb9c429246387a402050a569f66eba88b8048
Seth Battis <sethbattis@stmarksschool.org>
2016-09-25 20:20:49 -0400

    May actually have to squash these commits…

--- custom-global-navigation-flyout-items.js.php
+++ custom-global-navigation-flyout-items.js.php
@@ -5,30 +5,6 @@ require_once __DIR__ . '/common.inc.php';
 ?>
 var canvashack = {
     insertItem: function(menu, itemTitle, itemUrl) {
-        $('.ReactTrayPortal:has(' + menu + ') ul').append('<li class="ic-NavMenu-list-item"><a href="' + itemUrl + '" class="ic-NavMenu-list-item__link">' + itemTitle + '</a></li>');
-    },
-
-    insertItems() {
-        <?php
-
-        foreach ($pluginMetadata['ITEMS'] as $item) {
-            echo str_replace(
-                '@USER_ID',
-                $_REQUEST['current_user']['id'],
-                "this.insertItem('{$item['menu']}', '{$item['title']}', '{$item['url']}');" . PHP_EOL
-            );
-        }
-
-        ?>
-    }
-}
-<?php
-
-require_once __DIR__ . '/common.inc.php';
-
-?>
-var canvashack = {
-    insertItem: function(menu, itemTitle, itemUrl) {
         MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
         var observer = new MutationObserver(function (mutations, observer) {
             mutations.forEach(function(mutation) {
