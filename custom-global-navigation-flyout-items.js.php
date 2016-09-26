01750681691720ef79b1e6c2c3c1e6b72b9594a6
Seth Battis <sethbattis@stmarksschool.org>
2016-09-25 20:17:17 -0400

    Ye Gods!
    It's like I coded this on a different system and didn't bother to do a diff or something. Hypothetically.

--- custom-global-navigation-flyout-items.js.php
+++ custom-global-navigation-flyout-items.js.php
@@ -1,3 +1,8 @@
+<?php
+
+require_once __DIR__ . '/common.inc.php';
+
+?>
 var canvashack = {
     insertItem: function(menu, itemTitle, itemUrl) {
         $('.ReactTrayPortal:has(' + menu + ') ul').append('<li class="ic-NavMenu-list-item"><a href="' + itemUrl + '" class="ic-NavMenu-list-item__link">' + itemTitle + '</a></li>');
